be20734c062f1d74bf56640b741c9d4557e8e3c2
cryptoswarm <mokhtar.safir@outlook.com>
2021-08-30 22:52:28 -0400

    form is hidden by default , when user click on add the form is shown

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task'
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -13,8 +15,16 @@ export class AddTaskComponent implements OnInit {
   text: string = '';
   day: string = '';
   reminder: boolean = false;
+  showTaskTracker!: boolean;
+  subscription!: Subscription;
 
-  constructor() { }
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showTaskTracker = value));
+  }
+
+  //the value in the constructor will allow us to use *ngIf="showTaskTracker" in the html form so the form will be shown or not
 
   ngOnInit(): void {
   }
